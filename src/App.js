e4149e521dd21a054769210c67740d91ac501935
mao13132 <mao13132@gmail.com>
2024-02-17 18:30:45 +0300

    end project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  /* let count = 0; */
+  const [count, setCount] = useState(0);
+
+  const plusFunct = () => {
+    setCount(count + 1);
+  };
+
+  const minusFunct = () => {
+    setCount(count - 1);
+  };
+
+
   return (
     <div className="App">
         <div>
           <h2>Счетчик:</h2>
-          <h1>0</h1>
-          <button className='minus'>- Минус</button>
-          <button className='plus'>+ Плюс</button>
+          <h1>{count}</h1>
+          <button onClick={minusFunct} className='minus'>- Минус</button>
+          <button onClick={plusFunct} className='plus'>+ Плюс</button>
         </div>
     </div>
   );
